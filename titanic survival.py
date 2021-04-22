d36075d52f8d0d3a0c947cd078c5bec1a9fb969c
kostas karagiorgos <34107442+kostaskaragiorgos@users.noreply.github.com>
2021-04-22 09:59:59 +0300

    Update titanic survival.py

--- titanic survival.py
+++ titanic survival.py
@@ -145,6 +145,7 @@ class Titanicsurvival():
         self.faretext.delete(1.0, END)
         self.pclassstring.set("Select a Ticket class")
         self.sexstring.set("Select a Sex")
+        self.embarkedstring.set("Select a Port of Embarkation")
         self.noffammebtext.delete(1.0, END)
         self.nofparentstext.delete(1.0, END)
 
